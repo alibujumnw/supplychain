815b02381701382d47a41e1e6f46fc3eca5f9196
unknown <alimanyawi@gmail.com>
2025-02-24 15:59:59 +0200

    Admin

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173'], // Replace with your frontend port
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:5173'], // Replace with your frontend URL
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true
+    'supports_credentials' => true,
 ];
