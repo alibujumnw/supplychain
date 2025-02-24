54c110b6ff7b23d2d9203e9a1a928e9cac6ee011
unknown <alimanyawi@gmail.com>
2025-02-24 15:19:27 +0200

    Admin

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000',     // Your local frontend (adjust port as needed)
-        'http://127.0.0.1:3000',], // Replace with specific domains in production
+    'allowed_origins' => ['*'], 
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
--- routes/api.php
+++ routes/api.php
@@ -81,7 +81,7 @@ Route::post('change-password',[FarmerController::class,'change_farmer_password']
 
 Route::post('create-crop',[FarmerController::class,'crop_details']);
 Route::get('view-crop/{id}',[FarmerController::class,'view_crop']);
-Route::get('view-crops',[FarmerController::class,'view_all_crops']);    
+Route::get('view-crops',[FarmerController::class,'view_all_crops']);     
 Route::get('delete-crop/{id}',[FarmerController::class,'delete_crop']);
 
 Route::post('update-farmer-details',[FarmerController::class,'update_farmer']);
