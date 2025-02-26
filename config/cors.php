a61ff8cdd48fd8f33ca5614bf4d66e84a636b972
unknown <alimanyawi@gmail.com>
2025-02-26 14:33:57 +0200

    change to cors

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,10 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Replace with your frontend URL
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 86400,
     'supports_credentials' => true,
 ];
